b4a5bb7be52bd04eb4efd1aca4196fd971339b40
Tamási Máté <matetamasi@protonmail.com>
2024-07-12 17:07:24 +0200

    Full functionality of creating teams and students

--- src/main/kotlin/ui/CreateTeam.kt
+++ src/main/kotlin/ui/CreateTeam.kt
@@ -5,11 +5,15 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -38,8 +42,8 @@ fun CreateTeam(competition: Competition) {
                 for (team in teams.sortedBy { it.id }) {
                     stickyHeader {
                         Surface(color = Color.Cyan, modifier = Modifier.fillMaxWidth().wrapContentHeight()) {
-                            Row {
-                                Text(team.id.toString())
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                Text("${team.id}. csapat", modifier = Modifier.padding(end = 8.dp))
                                 IconButton(onClick = {
                                     teams.remove(team)
                                     for (i in 0..<teams.size) {
@@ -48,19 +52,23 @@ fun CreateTeam(competition: Competition) {
                                             teams.add(i, Team(t.id - 1, t.students, t.competition))
                                         }
                                     }
-                                }, modifier = Modifier.size(90.dp)) {
-                                    Icon(Icons.Filled.Delete, "Törlés")
+                                }, modifier = Modifier.size(40.dp)) {
+                                    Icon(Icons.Filled.Delete, "Csapat törlése")
                                 }
                             }
 
                         }
                     }
 
-                    itemsIndexed(team.students, key = { _, item -> item.id }) { index, item ->
-                        ReorderableItem(reorderableLazyListState, key = item.id) { isDragging ->
-                            // Item content
+                    itemsIndexed(team.students, key = { _, student -> student.id }) { index, student ->
+                        ReorderableItem(reorderableLazyListState, key = student.id) { isDragging ->
 
-                            StudentCard(item)
+                            StudentCard(student) {
+                                teams.remove(team)
+                                val students = team.students
+                                students.remove(student)
+                                teams.add(Team(team.id, students, team.competition))
+                            }
                         }
                     }
                     item {
@@ -69,30 +77,26 @@ fun CreateTeam(competition: Competition) {
                                 teams.remove(team)
                                 team.students.add(Student(""))
                                 teams.add(team)
-                            }
+                            },
                         ) {
-
+                            Row {
+                                Icon(Icons.Default.Person, "")
+                                Text("Csapattag hozzáadása")
+                            }
                         }
                     }
                 }
-            }
-        }
 
-        Column {
-            Button(
-                onClick = {
-                    val lastTeam = teams.last()
-                    teams.removeLast()
-                    lastTeam.students.add(Student(""))
-                    teams.add(lastTeam)
+                item {
+                    Button(shape = CircleShape, onClick = {
+                        teams.add(Team(viewModel.newTeamId(), mutableListOf(), competition))
+                    }) {
+                        Row {
+                            Icon(Icons.Default.Add, "", modifier = Modifier.size(50.dp))
+                            Text("Csapat hozzáadása")
+                        }
+                    }
                 }
-            ) {
-                Text("Add Student")
-            }
-            Button(onClick = {
-                teams.add(Team(viewModel.newTeamId(), mutableListOf(), competition))
-            }) {
-                Text("Add Team")
             }
         }
     }
--- src/main/kotlin/ui/StudentCard.kt
+++ src/main/kotlin/ui/StudentCard.kt
@@ -3,10 +3,11 @@ package ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -14,26 +15,36 @@ import androidx.compose.ui.unit.dp
 import model.Student
 
 @Composable
-fun StudentCard(s: Student) {
+fun StudentCard(s: Student, onDeleteStudent: () -> Unit) {
     var student by remember { mutableStateOf(s) }
 
-    Card {
+    Card(modifier = Modifier.padding(8.dp)) {
         Column {
-            TextField(
-                modifier = Modifier.padding(8.dp),
-                value = student.name,
-                onValueChange = { student = student.copy(name = it) },
-                enabled = true,
-                leadingIcon = { Icon(Icons.Filled.Person, "asdf") }
-            )
             Row {
                 TextField(
+                    modifier = Modifier.padding(8.dp),
+                    value = student.name,
+                    onValueChange = { student = student.copy(name = it) },
+                    enabled = true,
+                    leadingIcon = { Icon(Icons.Filled.Person, "Diák neve") },
+                    label = { Text("Diák neve") }
+                )
+                IconButton(onClick = onDeleteStudent) { Icon(Icons.Filled.Delete, "Diák törlése") }
+            }
+            Row {
+                TextField(
+                    modifier = Modifier.padding(8.dp),
                     value = student.group,
-                    onValueChange = { student = student.copy(group = it) }
+                    leadingIcon = { Icon(Icons.Filled.Home, "Csoport") },
+                    onValueChange = { student = student.copy(group = it) },
+                    label = { Text("Csoport") }
                 )
                 TextField(
+                    modifier = Modifier.padding(8.dp),
                     value = student.klass,
-                    onValueChange = { student = student.copy(klass = it) }
+                    leadingIcon = { Icon(Icons.Filled.DateRange, "Osztály") },
+                    onValueChange = { student = student.copy(klass = it) },
+                    label = { Text("Osztály") }
                 )
             }
         }
