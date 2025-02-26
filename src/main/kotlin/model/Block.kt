1ad5dfc69d957fbb2a3878e055df3cadf2ac6486
Tamási Máté <matetamasi@protonmail.com>
2025-02-26 23:08:49 +0100

    added identifier value to Task and Block

--- src/main/kotlin/model/Block.kt
+++ src/main/kotlin/model/Block.kt
@@ -1,11 +1,15 @@
 package model
 
-data class Block(val tasks: List<Task>, val minCorrectToProgress: Int){
+data class Block(
+    val tasks: List<Task>,
+    val minCorrectToProgress: Int,
+    val id: Int = nextId()
+) {
     fun getTotalTasks() = tasks.size
 
     private fun correctCount(answers: List<String>): Int {
         var correct = 0
-        for ((t,a) in tasks.zip(answers)) {
+        for ((t, a) in tasks.zip(answers)) {
             if (t.solution == a) {
                 correct++
             }
@@ -19,5 +23,13 @@ data class Block(val tasks: List<Task>, val minCorrectToProgress: Int){
 
     fun mustProgress(answers: List<String>): Boolean {
         return correctCount(answers) == tasks.size
+    }
+
+    companion object IdCounter {
+        private var id = 0
+        fun nextId(): Int {
+            id += 1
+            return id
+        }
     }
 }
--- src/main/kotlin/model/Task.kt
+++ src/main/kotlin/model/Task.kt
@@ -1,3 +1,15 @@
 package model
 
-data class Task( val text: String = "", val solution: String )
+data class Task(
+    val text: String = "",
+    val solution: String,
+    private val id: Int = nextId()
+) {
+    companion object IdCounter {
+        private var id = 0
+        fun nextId(): Int {
+            id += 1
+            return id
+        }
+    }
+}
