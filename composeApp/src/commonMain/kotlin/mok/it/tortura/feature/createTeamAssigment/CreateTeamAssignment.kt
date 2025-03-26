3becf136db7db2bf7ca79781f570ff2a3e084fb0
Tamási Máté <matetamasi@protonmail.com>
2025-03-27 00:00:37 +0100

    Added save button to CreateTeamAssignment

--- composeApp/src/commonMain/kotlin/mok/it/tortura/feature/createTeamAssigment/CreateTeamAssignment.kt
+++ composeApp/src/commonMain/kotlin/mok/it/tortura/feature/createTeamAssigment/CreateTeamAssignment.kt
@@ -14,11 +14,16 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import io.github.vinceglb.filekit.dialogs.compose.rememberFileSaverLauncher
+import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json
+import mok.it.tortura.saveStringToFile
 import mok.it.tortura.ui.components.StudentCard
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -29,64 +34,81 @@ fun CreateTeamAssignment(
     val viewModel = viewModel { CreateTeamAssignmentViewModel() }
     val teamAssignment by remember { viewModel.teamAssignment }
 
+    val scope = rememberCoroutineScope()
+    val launcher = rememberFileSaverLauncher { file ->
+        if (file != null) {
+            scope.launch {
+                saveStringToFile(file, Json.encodeToString(viewModel.teamAssignment.value))
+            }
+        }
+    }
     val lazyListState = rememberLazyListState()
 
     Row {
         Surface {
-            LazyColumn(state = lazyListState) {
-                for (team in teamAssignment.teams) {
-                    stickyHeader {
-                        Surface(color = Color.Cyan, modifier = Modifier.fillMaxWidth().wrapContentHeight()) {
-                            Row(verticalAlignment = Alignment.CenterVertically) {
-                                Text(
-                                    "${teamAssignment.teams.indexOf(team) + 1}. csapat",
-                                    modifier = Modifier.padding(end = 8.dp)
-                                )
-                                IconButton(onClick = {
-                                    viewModel.onEvent(CreateTeamAssignmentEvent.DeleteTeam(team))
-                                }, modifier = Modifier.size(40.dp)) {
-                                    Icon(Icons.Filled.Delete, "Csapat törlése")
+            Column {
+
+                LazyColumn(
+                    state = lazyListState,
+                    modifier = Modifier.weight(1f)
+                ) {
+                    for (team in teamAssignment.teams) {
+                        stickyHeader {
+                            Surface(color = Color.Cyan, modifier = Modifier.fillMaxWidth().wrapContentHeight()) {
+                                Row(verticalAlignment = Alignment.CenterVertically) {
+                                    Text(
+                                        "${teamAssignment.teams.indexOf(team) + 1}. csapat",
+                                        modifier = Modifier.padding(end = 8.dp)
+                                    )
+                                    IconButton(onClick = {
+                                        viewModel.onEvent(CreateTeamAssignmentEvent.DeleteTeam(team))
+                                    }, modifier = Modifier.size(40.dp)) {
+                                        Icon(Icons.Filled.Delete, "Csapat törlése")
+                                    }
                                 }
                             }
                         }
-                    }
 
-                    items(team.students) { student ->
-                        StudentCard(student) {
-                            viewModel.onEvent(CreateTeamAssignmentEvent.DeleteMember(team, student))
+                        items(team.students) { student ->
+                            StudentCard(student) {
+                                viewModel.onEvent(CreateTeamAssignmentEvent.DeleteMember(team, student))
+                            }
                         }
-                    }
 
+                        item {
+                            Button(
+                                onClick = {
+                                    viewModel.onEvent(CreateTeamAssignmentEvent.AddStudent(team))
+                                },
+                            ) {
+                                Row {
+                                    Icon(Icons.Default.Person, "")
+                                    Text("Csapattag hozzáadása")
+                                }
+                            }
+                        }
+
+                    }
                     item {
-                        Button(
-                            onClick = {
-                                viewModel.onEvent(CreateTeamAssignmentEvent.AddStudent(team))
-                            },
-                        ) {
+                        Button(shape = CircleShape, onClick = {
+                            println("Új csapat ${teamAssignment.teams.size}")
+                            viewModel.onEvent(CreateTeamAssignmentEvent.AddTeam)
+                        }) {
                             Row {
-                                Icon(Icons.Default.Person, "")
-                                Text("Csapattag hozzáadása")
+                                Icon(Icons.Default.Add, "", modifier = Modifier.size(50.dp))
+                                Text("Csapat hozzáadása")
                             }
                         }
                     }
-
-                }
-                item {
-                    Button(shape = CircleShape, onClick = {
-                        println( "Új csapat ${teamAssignment.teams.size}" )
-                        viewModel.onEvent(CreateTeamAssignmentEvent.AddTeam)
-                    }) {
-                        Row {
-                            Icon(Icons.Default.Add, "", modifier = Modifier.size(50.dp))
-                            Text("Csapat hozzáadása")
+                    item {
+                        Button(onClick = onNext) {
+                            Text("Tovább")
                         }
                     }
                 }
-                item {
-                    Button(onClick = onNext) {
-                        Text("Tovább")
-                    }
-                }
+                Button(
+                    onClick = { launcher.launch("output", "txt") }
+                ) { Text(text = "Mentés") }
             }
         }
     }
